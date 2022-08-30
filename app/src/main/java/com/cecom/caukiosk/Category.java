34b3d8544c333e09236585523eb762a5536d36f1
yymin1022 <yymin1022@gmail.com>
2022-08-31 03:30:54 +0900

    Update Category : Cleaned UP

--- app/src/main/java/com/cecom/caukiosk/Category.java
+++ app/src/main/java/com/cecom/caukiosk/Category.java
@@ -1,17 +1,17 @@
 package com.cecom.caukiosk;
 
+import androidx.annotation.NonNull;
+
 public class Category {
-    String cate = "";
-    String room = "";
+    String cate;
+    String room;
 
     Category(String cate, String room){
         this.cate = cate;
         this.room = room;
     }
-    Category(String cate){
-        this.cate = cate;
-    }
 
+    @NonNull
     @Override
     public String toString() {
         return this.cate;
