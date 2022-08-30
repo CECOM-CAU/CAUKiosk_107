17a8b6a3499f97274a7084eae0c612623d446a28
yymin1022 <yymin1022@gmail.com>
2022-08-31 03:32:43 +0900

    Update Permission Activity : Cleaned UP

--- app/src/main/java/com/cecom/caukiosk/PermissionActivity.java
+++ app/src/main/java/com/cecom/caukiosk/PermissionActivity.java
@@ -1,25 +1,13 @@
 package com.cecom.caukiosk;
 
-import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 
-
 public class PermissionActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +26,10 @@ public class PermissionActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if(KioskModeApp.isInLockMode == true){
+        if(KioskModeApp.isInLockMode){
             ActivityManager activityManager = (ActivityManager) getApplicationContext()
                     .getSystemService(Context.ACTIVITY_SERVICE);
             activityManager.moveTaskToFront(getTaskId(), 0);
         }
     }
-
 }
